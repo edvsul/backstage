5d68dbe3d1b2b7cf007e172ab8b263ef1d8ab9d0
edvsul <edvinas.sulzickis@gmail.com>
2025-04-21 16:43:04 +0200

    Add github plugins

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -52,4 +52,8 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
+// github
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
+
 backend.start();
